be95a790e3b75211b7b38018f8cf251bcc330ebe
OniMinetods <146983705+OniMinetods@users.noreply.github.com>
2025-07-21 06:02:42 +0300

    feat: task 3-4 completed

--- main.js
+++ main.js
@@ -33,3 +33,58 @@ disLikeButton.addEventListener('click', () => {
     console.log('Кнопка 2 деактивирована!');
   }
 });
+
+// Task 3
+
+const item = document.getElementById('img');
+let counter = 0;
+
+function addInto() {
+  counter++;
+  document.getElementById('counter-value').textContent = counter;
+  console.log('Товар добавлен в корзину!');
+}
+
+// Task 4
+
+let arr = [2, 4, 3, 5, 1];
+let arr2 = arr.slice();
+let outputDiv = document.getElementById('output');
+let listItems = outputDiv.querySelectorAll('li');
+arr.forEach((item) => {
+  const li = document.createElement('li');
+  li.textContent = item;
+  outputDiv.appendChild(li);
+});
+
+function sortUp() {
+  outputDiv.textContent = '';
+  arr.sort();
+  arr.forEach((item) => {
+    const li = document.createElement('li');
+    li.textContent = item;
+    outputDiv.appendChild(li);
+  });
+}
+
+function sortDown() {
+  outputDiv.textContent = '';
+  arr.sort();
+  arr.reverse();
+  arr.forEach((item) => {
+    const li = document.createElement('li');
+    li.textContent = item;
+    outputDiv.appendChild(li);
+  });
+}
+
+function sortDefault() {
+  outputDiv.textContent = '';
+  arr2.forEach((item) => {
+    const li = document.createElement('li');
+    li.textContent = item;
+    outputDiv.appendChild(li);
+  });
+}
+
+// Task 5
